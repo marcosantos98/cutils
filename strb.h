c64a2a5ad0bc55ce6b54dff38c909963ebee01f8
marcosantos98 <marcomsantos98@gmail.com>
2024-03-01 23:51:22 +0000

    Extended strb.h

--- strb.h
+++ strb.h
@@ -4,6 +4,14 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ___STRB_ENSURE_CAP___(strb, amt)                                                   \
+    do {                                                                                   \
+        if ((strb)->cnt + amt > (strb)->cap) {                                             \
+            (strb)->cap *= 2;                                                              \
+            (strb)->str = (char *)realloc((strb)->str, sizeof((strb)->str) * (strb)->cap); \
+        }                                                                                  \
+    } while (0)
+
 typedef struct {
     char *str;
     size_t cnt;
@@ -18,13 +26,14 @@ strb strb_init(int initial_cap) {
     };
 }
 
+void strb_append_single(strb *strb, char to_append) {
+    ___STRB_ENSURE_CAP___(strb, 1);
+    strb->str[strb->cnt++] = to_append;
+}
+
 void strb_append(strb *strb, const char *to_append) {
     size_t to_append_len = strlen(to_append);
-    if (strb->cnt + to_append_len > strb->cap) {
-        strb->cap *= 2;
-        strb->str = (char *)realloc(strb->str, sizeof(strb->str) * strb->cap);
-    }
-
+    ___STRB_ENSURE_CAP___(strb, to_append_len);
     memcpy(strb->str + strb->cnt, to_append, to_append_len);
     strb->cnt += to_append_len;
 }
--- strb_test.c
+++ strb_test.c
@@ -7,6 +7,9 @@ int main(void) {
     strb strb = strb_init(16);
     strb_append(&strb, "Hello World\n");
     strb_append(&strb, "... or mom!\n");
+    strb_append_single(&strb, '6');
+    strb_append_single(&strb, 10);
+    printf("%zu\n", strb.cap);
 
     printf("%s", strb.str);
 
